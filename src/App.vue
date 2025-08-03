b86f35957c75e8c2b9d4f80758cfe5fec99ed637
Anatoliy Vikulov <vikulovtolya38@gmail.com>
2025-08-04 03:10:58 +0800

    fix auth && add header

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,27 @@
 <script setup>
-import axios from 'axios'
-import { onBeforeMount } from 'vue'
+import { computed, onBeforeMount } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
+const visibleHeader = computed(
+  () =>
+    router.currentRoute.value.path !== '/login' || router.currentRoute.value.path !== '/register',
+)
 
 onBeforeMount(async () => {
-  try {
-    await axios.get('https://a16b7f641c9d755d.mokky.dev/auth_me')
-  } catch (error) {
-    console.log(error)
-    if (error.response.status === 401) {
-      router.push('/login')
-    } else {
-      router.push('/register')
-    }
+  if (!localStorage.getItem('token')) {
+    router.push('/login')
   }
 })
 </script>
 
 <template>
+  <header v-if="visibleHeader" class="px-5 py-2 rounded bg-whiteh-[30px] flex items-center">
+    <ul class="flex gap-3">
+      <li class="text-[#609aff] text-md">Профиль</li>
+      <li class="text-[#609aff] text-md">Пользователи</li>
+    </ul>
+  </header>
   <div class="wrapper">
     <router-view></router-view>
   </div>
