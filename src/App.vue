21180a86901a846ec9cb85f0cb7c2bd9e7d211f6
Anatoliy Vikulov <vikulovtolya38@gmail.com>
2025-08-04 02:42:41 +0800

    update auth logic

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,23 @@
-<script setup></script>
+<script setup>
+import axios from 'axios'
+import { onBeforeMount } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onBeforeMount(async () => {
+  try {
+    await axios.get('https://a16b7f641c9d755d.mokky.dev/auth_me')
+  } catch (error) {
+    console.log(error)
+    if (error.response.status === 401) {
+      router.push('/login')
+    } else {
+      router.push('/register')
+    }
+  }
+})
+</script>
 
 <template>
   <div class="wrapper">
